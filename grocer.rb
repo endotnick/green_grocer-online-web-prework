e5c0f50e731f119464636acdc4b8472dc92cea64
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 05:22:37 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -3,14 +3,15 @@ def consolidate_cart(cart)
 
   cart.each do |items|
     items.each do |item, data|
-      small_cart[item] = data.merge({:count => cart.count(items)})
+      small_cart[item] = data.merge({ :count => cart.count(items) })
     end
-  end    
+  end
   small_cart
 end
 
 def apply_coupons(cart, coupons)
-
+  coupons.each do |coupon|
+    coupon.each do |
 end
 
 def apply_clearance(cart)
@@ -18,5 +19,5 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-
+  cart = consolidate_cart(cart).apply_coupons(coupons).apply_clearance
 end
